56520b188d17ebc93ef3063acb81e8c99e0d55a6
Mark W <markwilkinson.141@gmail.com>
2023-05-11 21:23:55 -0400

    Testing vt100 escape codes, don't think this will work long term, need to find new way to write to terminal, will use inspiration

---
+++ testing.nim
@@ -0,0 +1,7 @@
+echo "This is a test"
+echo "\eH 123 another test" #Tabset, idk what this does, might be useful
+echo "Another test too"
+#stdout.write "\eM" #makes another test too disapear
+#stdout.write "\eD" #Makes new line
+#stdout.write "\e[1A" #also makes another test too disapear
+
